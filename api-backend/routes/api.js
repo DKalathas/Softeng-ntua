b5c4867daadc95e8f0e9f4ad09abb0f928844dcb
Dimitrios Kalemis <dimitrios.kalemis@nesicon.com>
2022-11-24 00:40:16 +0200

    add questionnaire via postman

--- api-backend/routes/api.js
+++ api-backend/routes/api.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router =  express.Router();
-const Questionnaire= require('../models/questionnaire');
-const controllers=require('../controllers/controllers');
+const Questionnaire = require('../models/questionnaire');
+const controllers = require('../controllers/controllers');
 
-
+// add new questionnaire to db
+router.post("/admin/questionnaire_upd", controllers.post_questionnaire);
 
 module.exports = router;
