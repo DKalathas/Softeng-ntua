0af8be13e7602c9bd1e7f27977ec07120701f8af
Jim Kalathas <tassos@Jim-MacBook-Air.local>
2022-11-28 13:48:50 +0200

    healthcheck end

--- api-backend/controllers/controllers.js
+++ api-backend/controllers/controllers.js
@@ -6,6 +6,12 @@ const post_questionnaire = (req, res, next) => {
         res.send(questionnaire);
     }).catch(next);
 }
+const get_healthcheck =(req,res)=>{
+    mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result=>res.send({"status":"ok","dbconnection":process.env.DATABASE_URI}))
+    .catch(err=>{
+        res.send({"status":"failed", "dbconnection":process.env.DATABASE_URI})})
+        }
 
 module.exports={
     post_questionnaire
