49058761d08b9b22e112f96b1bc58c839f3bf94d
Jim Kalathas <tassos@Jim-MacBook-Air.local>
2022-11-28 13:47:13 +0200

    healthckeck

--- api-backend/routes/api.js
+++ api-backend/routes/api.js
@@ -3,6 +3,10 @@ const router =  express.Router();
 const Questionnaire = require('../models/questionnaire');
 const controllers = require('../controllers/controllers');
 
+
+//healthcheck
+router.get("/admin/healthcheck", controllers.get_healthcheck);
+
 // add new questionnaire to db
 router.post("/admin/questionnaire_upd", controllers.post_questionnaire);
 
