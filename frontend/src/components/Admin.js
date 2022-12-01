e8ee8223ebe94d1e38e6b87f1c28bf8347ae82d1
gr3gor1 <gr3gorypapanikolaou@gmail.com>
2022-12-01 19:11:06 +0200

    started

---
+++ frontend/src/components/Admin.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+import './Admin.css';
+
+const Admin = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className='Admin'>
+            <div className='row heading'>
+                <h1 className='text-center col'>Administrative Actions</h1>
+                <button className='col text-center home' onClick={()=>navigate(-1)}>Go Back Home</button>
+            </div>
+            <div className='row'>  
+            <div className='col col1'>
+                <div class="card-body crd1 text-center">
+                        <p class="card-text">This button resets the database and archives all the data.</p>
+                    <button class="btn btn2 wobble-hor-top">Reset All</button>
+                </div>
+                <div class="card-body crd1 text-center">
+                        <p class="card-text">This button checks whether the connection to the database and the api is up.</p>
+                    <button class="btn btn2 wobble-hor-top">Healtcheck</button>
+                </div>
+                <div class="card-body crd1 text-center">
+                        <p class="card-text">This button resets the questionnaire as well as all the answers given.</p>
+                    <button class="btn btn2 wobble-hor-top">Reset questionnaire</button>
+                </div>
+                <div class="card-body crd1 text-center">
+                        <p class="card-text">This button lets you create a new questionnaire.</p>
+                    <button class="btn btn2 wobble-hor-top" onClick={()=>navigate("/upload")}>Upload New Questionnaire</button>
+                </div>
+            </div>
+            </div>
+         </div>
+    )
+};
+
+export default Admin;
