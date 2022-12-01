5e9e9e4313a874d02ce0c7c9e8733b3cefa2efa9
gr3gor1 <gr3gorypapanikolaou@gmail.com>
2022-12-01 21:25:57 +0200

    added answer schema - added post answer

--- api-backend/controllers/controllers.js
+++ api-backend/controllers/controllers.js
@@ -1,5 +1,6 @@
 const Questionnaire = require("../models/questionnaire");
-
+const Answer = require("../models/answers")
+const mongoose = require('mongoose');
 
 const post_questionnaire = (req, res, next) => {
     Questionnaire.create(req.body).then(function(questionnaire) {
@@ -16,7 +17,24 @@ const resetall = (req,res,next)=>{
         }
     });
 }
+
+const get_healthcheck =(req,res)=>{
+    mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result=>res.send({"status":"ok","dbconnection":process.env.DATABASE_URI}))
+    .catch(err=>{
+        res.send({"status":"failed", "dbconnection":process.env.DATABASE_URI})})
+        }
+
+const addAnswer = async (req, res, next) => {
+    Answer.create(req.body).then(function(answer) {
+        res.send(answer);
+    }).catch(next);
+  };
+  
+
 module.exports={
     post_questionnaire,
-    resetall
+    resetall,
+    get_healthcheck,
+    addAnswer
 }
