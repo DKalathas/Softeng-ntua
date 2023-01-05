8d01318c458a7699640e85ef6c76ea00470e2823
Jim Kalathas <tassos@Jim-MacBook-Air.local>
2023-01-05 14:48:52 +0200

    cli update with format

--- cli/cli.py
+++ cli/cli.py
@@ -16,69 +16,125 @@ def cli():
     pass
 
 @click.command()
-def healthcheck():
+@click.option('--format', help = 'Choose format (json or csv)', required = 'True', metavar = '[csv/json]')
+def healthcheck(format):
 
-    res = requests.get('http://localhost:4000/intelliq_api/admin/healthcheck')
+    url='http://localhost:4000/intelliq_api/admin/healthcheck'
+    
+
+    url=url+'?format='+str(format)
+    res = requests.get(url)
     click.echo(res.status_code)
-    click.echo(res.json())
+    if (format != "csv" and format !='json'):
+        click.echo(res.content)
+    if(format == 'json' and res.status_code == 200):
+        click.echo(json.dumps(res.json()))
+    elif(format == 'csv' and res.status_code == 200):
+        click.echo(res.content)
+    return True
     
     
 @click.command()
-def resetall():
+@click.option('--format', help = 'Choose format (json or csv)', required = 'True', metavar = '[csv/json]')
+def resetall(format):
 
-
-    res = requests.post('http://localhost:4000/intelliq_api/admin/resetall')
+    url='http://localhost:4000/intelliq_api/admin/resetall'
+    
+    url=url+'?format='+str(format)
+    res = requests.post(url)
     click.echo(res.status_code)
-    click.echo(res.json())
+    if (format != "csv" and format !='json'):
+        click.echo(res.content)
+    if(format == 'json' and res.status_code == 200):
+        click.echo(json.dumps(res.json()))
+    elif(format == 'csv' and res.status_code == 200):
+        click.echo(res.content)
+    return True
    
     
 @click.command()
 @click.option('--source', help = 'Upload a file', required = 'True', metavar = '<filename>')
-#@click.option('--format', help = 'Choose format (json or csv)', required = 'True', metavar = '[csv/json]')
-def questionnaire_upd(source):
+@click.option('--format', help = 'Choose format (json or csv)', required = 'True', metavar = '[csv/json]')
+def questionnaire_upd(source,format):
 
     f = open(source)
     
     mydata = json.load(f)
-    
-    res = requests.post('http://localhost:4000/intelliq_api/admin/questionnaire_upd',
+    url='http://localhost:4000/intelliq_api/admin/questionnaire_upd'
+    url=url+'?format='+str(format)
+
+    res = requests.post(url,
                 json=mydata)
     click.echo(res.status_code)
-    click.echo(res.json())
+    if (format != "csv" and format !='json'):
+        click.echo(res.content)
+    if(format == 'json' and res.status_code == 200):
+        click.echo(json.dumps(res.json()))
+    elif(format == 'csv' and res.status_code == 200):
+        click.echo(res.content)
+    
     f.close()
+    return True
 
 @click.command()
 @click.option('--questionnaire_id', help = 'delete all answers from a questionnaire', required = 'True', metavar = '<questionnaire_id>')
-def resetq(questionnaire_id):
+@click.option('--format', help = 'Choose format (json or csv)', required = 'True', metavar = '[csv/json]')
+def resetq(questionnaire_id,format):
     
     
     url='http://localhost:4000/intelliq_api/admin/resetq/'+questionnaire_id
+    url=url+'?format='+str(format)
+
     res = requests.post(url)
     click.echo(res.status_code)
-    click.echo(res.json())
+    if (format != "csv" and format !='json'):
+        click.echo(res.content)
+    if(format == 'json' and res.status_code == 200):
+        click.echo(json.dumps(res.json()))
+    elif(format == 'csv' and res.status_code == 200):
+        click.echo(res.content)
+    return True
 
 
 @click.command()
 @click.option('--questionnaire_id', help = 'Find a questionnaire', required = 'True', metavar = '<questionnaire_id>')
-def questionnaire(questionnaire_id):
+@click.option('--format', help = 'Choose format (json or csv)', required = 'True', metavar = '[csv/json]')
+def questionnaire(questionnaire_id,format):
     
     
     url='http://localhost:4000/intelliq_api/questionnaire/'+questionnaire_id
+    url=url+'?format='+str(format)
+
     res = requests.get(url)
     click.echo(res.status_code)
-    click.echo(res.json())
+    if (format != "csv" and format !='json'):
+        click.echo(res.content)
+    if(format == 'json' and res.status_code == 200):
+        click.echo(json.dumps(res.json()))
+    elif(format == 'csv' and res.status_code == 200):
+        click.echo(res.content)
+    return True
     
 
 @click.command()
 @click.option('--questionnaire_id', help = 'Find a questionnaire', required = 'True', metavar = '<questionnaire_id>')
 @click.option('--question_id', help = 'Find a question', required = 'True', metavar = '<question_id>')
-def question(questionnaire_id,question_id):
+@click.option('--format', help = 'Choose format (json or csv)', required = 'True', metavar = '[csv/json]')
+def question(questionnaire_id,question_id,format):
     
     
     url='http://localhost:4000/intelliq_api/question/'+questionnaire_id+'/'+question_id
+    url=url+'?format='+str(format)
+
     res = requests.get(url)
     click.echo(res.status_code)
-    click.echo(res.json())
+    if (format != "csv" and format !='json'):
+        click.echo(res.content)
+    if(format == 'json' and res.status_code == 200):
+        click.echo(json.dumps(res.json()))
+    elif(format == 'csv' and res.status_code == 200):
+        click.echo(res.content)
+    return True
 
 
 @click.command()
@@ -86,38 +142,64 @@ def question(questionnaire_id,question_id):
 @click.option('--question_id', help = 'Find a question', required = 'True', metavar = '<question_id>')
 @click.option('--session_id', help = 'Find a session', required = 'True', metavar = '<session_id>')
 @click.option('--option_id', help = 'Find a option', required = 'True', metavar = '<option_id>')
-def doanswer(questionnaire_id,question_id,session_id,option_id):
+@click.option('--format', help = 'Choose format (json or csv)', required = 'True', metavar = '[csv/json]')
+def doanswer(questionnaire_id,question_id,session_id,option_id,format):
     
     
     url='http://localhost:4000/intelliq_api/doanswer/'+questionnaire_id+'/'+question_id+'/'+session_id+'/'+option_id
+    url=url+'?format='+str(format)
+
     res = requests.post(url)
     click.echo(res.status_code)
-    click.echo(res.json())
+    if (format != "csv" and format !='json'):
+        click.echo(res.content)
+    if(format == 'json' and res.status_code == 200):
+        click.echo(json.dumps(res.json()))
+    elif(format == 'csv' and res.status_code == 200):
+        click.echo(res.content)
+    return True
 
 
 
 @click.command()
 @click.option('--questionnaire_id', help = 'Find a questionnaire', required = 'True', metavar = '<questionnaire_id>')
 @click.option('--session_id', help = 'Find a session', required = 'True', metavar = '<session_id>')
-def getsessionanswers(questionnaire_id,session_id):
+@click.option('--format', help = 'Choose format (json or csv)', required = 'True', metavar = '[csv/json]')
+def getsessionanswers(questionnaire_id,session_id,format):
     
     
     url='http://localhost:4000/intelliq_api/getsessionanswers/'+questionnaire_id+'/'+session_id
+    url=url+'?format='+str(format)
+
     res = requests.get(url)
     click.echo(res.status_code)
-    click.echo(res.json())
-    
+    if (format != "csv" and format !='json'):
+        click.echo(res.content)
+    if(format == 'json' and res.status_code == 200):
+        click.echo(json.dumps(res.json()))
+    elif(format == 'csv' and res.status_code == 200):
+        click.echo(res.content)
+    return True
     
 @click.command()
 @click.option('--questionnaire_id', help = 'Find a questionnaire', required = 'True', metavar = '<questionnaire_id>')
 @click.option('--question_id', help = 'Find a question', required = 'True', metavar = '<question_id>')
-def getquestionanswers(questionnaire_id,question_id):
+@click.option('--format', help = 'Choose format (json or csv)', required = 'True', metavar = '[csv/json]')
+def getquestionanswers(questionnaire_id,question_id,format):
     
     
     url='http://localhost:4000/intelliq_api/getquestionanswers/'+questionnaire_id+'/'+question_id
+    url=url+'?format='+str(format)
+
     res = requests.get(url)
     click.echo(res.status_code)
-    click.echo(res.json())
+    if (format != "csv" and format !='json'):
+        click.echo(res.content)
+    if(format == 'json' and res.status_code == 200):
+        click.echo(json.dumps(res.json()))
+    elif(format == 'csv' and res.status_code == 200):
+        click.echo(res.content)
+    return True
 
 
 
