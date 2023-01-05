de689df6e6e5411807112d1157353cb2a0293064
ntua-el18016 <el18016@mail.ntua.gr>
2023-01-05 21:27:29 +0200

    first try

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Admin from "./components/Admin";
-import Upload from './components/Upload';
+import All from './components/ALL';
+import QuestionAll from "./components/QuestionsAll";
 
 function App() {
 
   return (
-      <>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/admin" element={<Admin/>}/>
-            <Route path='/upload' element={<Upload/>}/>
-          </Routes>
-        </BrowserRouter>
-      </>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path='/all' element={<All />} />
+          <Route path="/questionnaire/:id" element={<QuestionAll />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
