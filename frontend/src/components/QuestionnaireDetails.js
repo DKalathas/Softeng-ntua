1d628bf5a2be450d0f0a3bc9f6b1aac0e4c2b9b1
Jim Kalathas <tassos@Jim-MacBook-Air.local>
2023-01-06 13:48:00 +0200

    a better try :)

--- frontend/src/components/QuestionnaireDetails.js
+++ frontend/src/components/QuestionnaireDetails.js
@@ -1,16 +1,38 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
-const QuestionnaireDetails = ({ questionnaire }) => {
+const QuestionnaireDetails = () => {
+
+    const { id } = useParams();
+    const { data: questionnaire, error, isPending } = useFetch('http://localhost:4000/intelliq_api/allquestionnaire/' + id);
     const navigate = useNavigate();
-    console.log(questionnaire)
-    return (
-        <div className="questionnaire-details">
-            <h1>hello</h1>
-            <h2>Title:{console.log(questionnaire.questionnaireID)}</h2>
-            <p>lol {questionnaire.questionnaireTitle}</p>
-            <div>he {questionnaire.questionnaireTitle}</div>
 
-            <button onClick={() => navigate('/admin')}>Back</button>
+    return (
+        <div className="questionnaire-details" >
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {questionnaire && questionnaire.map(questionnaire => (
+                <div key={questionnaire.questionnaireID}>
+                    <button onClick={() => navigate(-1)}>Back</button>
+                    <h2 >ID of Questionnaire: {questionnaire.questionnaireID}</h2>
+                    <p>Title of Questionnaire: {questionnaire.questionnaireTitle}</p>
+                    {questionnaire.questions && questionnaire.questions.map(questions => (
+                        <div className="questionnaire-preview" key={questions.qID}>
+                            <p >ID of Question: {questions.qID}</p>
+                            <p>Question: {questions.qtext}</p>
+                            {questions.options && questions.options.map(option => (
+                                <div className='box' key={option.optID}>
+                                    <p>ID of Option: {option.optID}</p>
+                                    <p>Option: {option.opttxt}</p>
+                                    <p>Next Qyestion: {option.nextqID}</p>
+
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                    <button onClick={() => navigate(-1)}>Back</button>
+                </div>
+            ))}
         </div>
     );
 }
