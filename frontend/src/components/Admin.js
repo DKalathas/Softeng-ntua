80a2c2331140e2cf0f74b3eb6d05d02887e8d362
gr3gor1 <el18649@mail.ntua.gr>
2023-01-07 17:04:44 +0200

    added ui elements

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -9,28 +9,23 @@ const Admin = () => {
         <div className='Admin'>
             <div className='row heading'>
                 <h1 className='text-center col'>Administrative Actions</h1>
-                <button className='col text-center home' onClick={() => navigate(-1)}>Go Back Home</button>
+                <button className='col text-center home' onClick={() => navigate("/")}>Go Back Home</button>
             </div>
             <div className='row'>
                 <div className='col col1'>
                     <div class="card-body crd1 text-center">
-                        <p class="card-text">This button resets the database and archives all the data.</p>
                         <button class="btn btn2 heartbeat">Reset All</button>
                     </div>
                     <div class="card-body crd1 text-center">
-                        <p class="card-text">This button checks whether the connection to the database and the api is up.</p>
                         <button class="btn btn2 heartbeat">Healtcheck</button>
                     </div>
                     <div class="card-body crd1 text-center">
-                        <p class="card-text">This button resets the questionnaire as well as all the answers given.</p>
                         <button class="btn btn2 heartbeat">Reset questionnaire</button>
                     </div>
                     <div class="card-body crd1 text-center">
-                        <p class="card-text">This button lets you review all questionnaires.</p>
                         <button class="btn btn2 heartbeat" onClick={() => navigate("/all")}>View All Questionnaires</button>
                     </div>
                     <div class="card-body crd1 text-center">
-                        <p class="card-text">This button lets you review all Answers from a Questionaire.</p>
                         <button class="btn btn2 heartbeat" onClick={() => navigate("/allanswers")}>View All Answers</button>
                     </div>
                 </div>
