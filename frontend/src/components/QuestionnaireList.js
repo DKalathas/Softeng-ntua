c34dd5c53811cf8a2b6a95395b38573c12133ae2
gr3gor1 <el18649@mail.ntua.gr>
2023-01-07 19:34:39 +0200

    added ui elements

--- frontend/src/components/QuestionnaireList.js
+++ frontend/src/components/QuestionnaireList.js
@@ -1,17 +1,23 @@
 import { Link } from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
 
 
 const QuestionnaireList = ({ questionnaire }) => {
     return (
         <div className="questionnaire-list">
+            <Carousel>
             {questionnaire.map(questionnaire => (
+                <Carousel.Item>
                 <div className="questionnaire-preview" key={questionnaire.questionnaireID} >
                     <Link to={`/questionnaire/${questionnaire.questionnaireID}`}>
-                        <h2>{questionnaire.questionnaireID}</h2>
-                        <p>{questionnaire.questionnaireTitle}</p>
+                        <h3 className='ID-car'>{questionnaire.questionnaireID}</h3>
+                        <p className='Title-car'><b>{"Title : " + questionnaire.questionnaireTitle}</b></p>
+                        <p className='Key-car'><b>{"Keywords : " + questionnaire.keywords.join(", ")}</b></p>
                     </Link>
                 </div>
+                </Carousel.Item>
             ))}
+            </Carousel>
         </div>);
 }
 
