7d37e91d8e23b3bc02e27ed16adc0bae8b76faaf
gr3gor1 <el18649@mail.ntua.gr>
2023-01-08 16:37:37 +0200

    added ui elements

--- frontend/src/components/AnswerList.js
+++ frontend/src/components/AnswerList.js
@@ -1,17 +1,25 @@
 import { Link } from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
+
 
 const AnswerList = ({ questionnaire }) => {
-    return (
-        <div className="questionnaire-list">
-            {questionnaire.map(questionnaire => (
-                <div className="questionnaire-preview" key={questionnaire.questionnaireID} >
-                    <Link to={`/answer/${questionnaire.questionnaireID}`}>
-                        <h2>{questionnaire.questionnaireID}</h2>
-                        <p>{questionnaire.questionnaireTitle}</p>
-                    </Link>
-                </div>
-            ))}
-        </div>);
+    return (        
+    <div className="questionnaire-list">
+    <Carousel className="car" interval={null}>
+    {questionnaire.map(questionnaire => (
+        <Carousel.Item>
+        <div className="questionnaire-preview" key={questionnaire.questionnaireID} >
+            <Link to={`/answer/${questionnaire.questionnaireID}`}>            
+                <h3 className='ID-car'>{questionnaire.questionnaireID}</h3>
+                <p className='Title-car'><b>{"Title : " + questionnaire.questionnaireTitle}</b></p>
+                <p className='Key-car'><b>{"Keywords : " + questionnaire.keywords.join(", ")}</b></p>
+            </Link>
+        </div>
+        </Carousel.Item>
+    ))}
+    </Carousel>
+</div>);
 }
 
 export default AnswerList;
+
