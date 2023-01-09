415325c688dffb2588063be3b184ad0abcf7f526
Jim Kalathas <tassos@Jim-MacBook-Air.local>
2023-01-09 21:47:31 +0200

    first try

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ import AllAnswers from "./components/AllAnswers";
 import AnswerDetails from "./components/AnswerDetails";
 import QuestionDetails from "./components/QuestionDetails";
 import Healthcheck from "./components/Healthcheck";
+import Ques from "./components/User";
+import QuesList from "./components/QuesList";
+import QuesDetails from "./components/QuesDetails";
 
 
 function App() {
@@ -24,6 +27,8 @@ function App() {
           <Route path="/healthcheck" element={<Healthcheck />} />
           <Route path="/answer/:id" element={<AnswerDetails />} />
           <Route path="/answer/:id/:qid/:opt" element={<QuestionDetails />} />
+          <Route path='/user' element={<Ques />} />
+          <Route path="/ques/:idsession/:qid" element={<QuesDetails />} />
         </Routes>
       </BrowserRouter>
     </>
