69b83efa1218b5ae77d21776f91e67f760830ee0
Jim Kalathas <el18016@ntua.mail.gr>
2023-01-10 18:08:55 +0200

    almost ready

--- frontend/src/components/Healthcheck.js
+++ frontend/src/components/Healthcheck.js
@@ -6,18 +6,17 @@ import { useNavigate } from "react-router-dom";
 const Healthcheck = () => {
     const navigate = useNavigate();
     const { data: stat, isPending, error } = useFetch('http://localhost:4000/intelliq_api/admin/healthcheck')
-    console.log(typeof stat)
-    return(
+    return (
         <div>
-        {isPending && <div className='text-center'>Loading...</div>}
-        {error && <div>{error}</div>}
-        {stat && <div className="text-center car8"><p><h2>Everything seems <b>{stat.status}</b> !!</h2></p></div>}
-        <div className="questionnaire-details">
-            <div className="text-center car6">
-            <button onClick={() => navigate(-1)}>Back</button>
+            {isPending && <div className='text-center'>Loading...</div>}
+            {error && <div>{error}</div>}
+            {stat && <div className="text-center car8"><p><h2>Everything seems <b>{stat.status}</b> !!</h2></p></div>}
+            <div className="questionnaire-details">
+                <div className="text-center car6">
+                    <button onClick={() => navigate(-1)}>Back</button>
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
     );
 }
 
